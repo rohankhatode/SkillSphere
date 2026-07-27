00fbd1e3894db366a41d72728ce0ec2f14954d95
Rohan Khatode <rohankhatode5@gmail.com>
2026-07-27 15:57:48 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Login from "./Components/Login/Login";
 import Home from "./pages/Home"
 
 function App() {
   return (
+    <>
     <Home />
+    <Login />
+    </>
   );
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+
+import Login from "../Components/Login/Login";
+
+
+function Home() {
+  return (
+    <>
+        <Login />
+    </>
+  );
+}
+
+export default Home;
